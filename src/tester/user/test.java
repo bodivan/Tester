262bf09443a6bf2bb8e3ef3ec1f950cc022d51c8
MyroslavNadj <skym1@github.com>
2013-01-08 00:35:33 +0200

    ///

---
+++ src/tester/user/test.java
@@ -0,0 +1,11 @@
+package tester.user;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Myroslav
+ * Date: 08.01.13
+ * Time: 0:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
