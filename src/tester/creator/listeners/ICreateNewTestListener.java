d5a9b74a60c26d1b84cebd22d5ee969d6f2627bb
MyroslavNadj <skym1@github.com>
2013-01-08 10:53:07 +0200

    s/

---
+++ src/tester/creator/listeners/ICreateNewTestListener.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tester.creator.listeners;
+
+/**
+ *
+ * @author Myroslav
+ */
+public interface ICreateNewTestListener {
+    public void testCreated(String testName, String fileLocation);
+}
